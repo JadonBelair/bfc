092678688e07e1962b934bd4b197c80a9edc15ce
Jadon <46850171+JadonBelair@users.noreply.github.com>
2024-01-20 16:00:19 -0500

    created Lexer struct

---
+++ src/lexer/mod.rs
@@ -0,0 +1,45 @@
+use std::{iter::Enumerate, str::Chars};
+
+pub struct Lexer<'a> {
+    source: Enumerate<Chars<'a>>,
+    current_char: char,
+    current_pos: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: &'a str) -> Self {
+        let mut l = Self {
+            source: source.chars().enumerate(),
+            current_char: '\0',
+            current_pos: 0,
+        };
+
+        l.next();
+
+        return l;
+    }
+
+    fn is_valid_char(&self) -> bool {
+        self.current_char == '+'
+            || self.current_char == '-'
+            || self.current_char == '<'
+            || self.current_char == '>'
+            || self.current_char == '.'
+            || self.current_char == ','
+            || self.current_char == '['
+            || self.current_char == ']'
+    }
+
+    fn next(&mut self) {
+        if let Some((i, c)) = self.source.next() {
+            self.current_char = c;
+            self.current_pos = i;
+
+            if !self.is_valid_char() {
+                self.next();
+            }
+        } else {
+            self.current_char = '\0';
+        }
+    }
+}
